e29ec38524581b76c2ec738305e2cee2694a41ca
itstraderbaby <itstraderbaby@gmail.com>
2025-12-07 18:40:51 -0500

    chore: fix console errors and migrate tailwind

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'
+import './index.css';
 import { registerSW } from 'virtual:pwa-register';
 
 // Register PWA Service Worker
